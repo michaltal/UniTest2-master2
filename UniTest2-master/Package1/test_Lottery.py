75d57362ef644789a693d6843cf7857fb1e16d69
michaltal <michaltal1@gmail.com>
2021-05-01 16:50:36 +0300

    updated

--- UniTest2-master/Package1/test_Lottery.py
+++ UniTest2-master/Package1/test_Lottery.py
@@ -2,18 +2,23 @@ from unittest import TestCase, mock
 from unittest.mock import patch
 from Package1.Lottery import Lottery
 
+
 class TestLottery(TestCase):
 
     def setUp(self):
-        self.lottery = Lottery()
         print('setUp')
+        self.lottery = Lottery()
 
     def tearDown(self):
         print('tearDown')
 
+    def test_rand_numbers(self):
+        self.assertTrue(self.lottery.rand_numbers())
+        self.assertTrue(len(self.lottery.rand_numbers()) != 0)
+        # self.assertIn('3', self.lottery.rand_numbers())
 
     # using mock as a decorator
-    @mock.patch('Package1.Lottery.Lottery.rand_numbers', return_value=[10,31,11,20,67,20])
+    @mock.patch('Package1.Lottery.Lottery.rand_numbers', return_value=[10, 31, 11, 20, 67, 20])
     def test_valid_numbers_False(self, mock_rand_numbers):
         # Tests a case in which a number is duplicated
         self.assertFalse(self.lottery.valid_numbers())
@@ -25,9 +30,9 @@ class TestLottery(TestCase):
         self.assertTrue(self.lottery.valid_numbers())
         print(self.lottery.numbers)
 
-    def test_valid_numbers_out_of_range(self):
+    def test_valid_range_out_of_range(self):
         # using mock as a context manager
-        with patch ('Package1.Lottery.Lottery.rand_numbers') as mock_rand_num:
+        with patch('Package1.Lottery.Lottery.rand_numbers') as mock_rand_num:
             mock_rand_num.return_value = [0, 31, 11, 12, 12, 20]
             print("out of range values", mock_rand_num.return_value)
             self.assertFalse(self.lottery.valid_range())
@@ -35,11 +40,9 @@ class TestLottery(TestCase):
             print("out of range values", mock_rand_num.return_value)
             self.assertFalse(self.lottery.valid_range())
 
-    def test_valid_numbers_in_range(self):
+    def test_valid_range_in_range(self):
         # using mock as a context manager
         with patch('Package1.Lottery.Lottery.rand_numbers') as mock_rand_num:
             mock_rand_num.return_value = [1, 31, 11, 12, 45, 20]
-            print("in range values", mock_rand_num.return_value )
+            print("in range values", mock_rand_num.return_value)
             self.assertTrue(self.lottery.valid_range())
-
-
